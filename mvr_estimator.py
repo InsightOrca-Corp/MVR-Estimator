01cf19537346fbaf4403f5f28f6a7e3cb73a595c
marisolh-io <marisolh@insightorca.com>
2025-10-31 14:58:43 -0700

    Removed unused imports

--- mvr_estimator.py
+++ mvr_estimator.py
@@ -4,11 +4,9 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 from io import BytesIO
 from model_backend import compute_mvr
 from datetime import datetime
-import matplotlib.pyplot as plt
 from PIL import Image
 
 # -----------------------------
